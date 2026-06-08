2aac29d75139ff6e2eb7ae10db8fcff8a659ba65
Adv-2005 <advarshney2005@gmail.com>
2026-06-08 18:57:32 +0530

    fixed embedding code

--- pdf-qa-app/src/embeddings.py
+++ pdf-qa-app/src/embeddings.py
@@ -1,5 +1,6 @@
 from langchain_ollama import OllamaEmbeddings
 
-embeddings = OllamaEmbeddings(
-    model="nomic-embed-text"
-)
+def get_embeddings():
+    return OllamaEmbeddings(
+        model="nomic-embed-text"
+    )
