a86fd99111f3c3ff2152a0c0a2532d45e8e39b6d
Adv-2005 <advarshney2005@gmail.com>
2026-06-12 12:44:19 +0530

    add structured grading for secondary verification and explicit web search routing

--- pdf-qa-app/src/graph_nodes.py
+++ pdf-qa-app/src/graph_nodes.py
@@ -148,6 +148,7 @@ If at least one document contains information that would help answer the questio
 return "yes".
 
 Return "no" only if none of the retrieved context is relevant.
+Return "no" if the user explicitly asks for web search,look on the internet, search online etc
 
 Question:
 {question}
@@ -251,10 +252,13 @@ Search Results:
 
 def answer_validation_node(state: GraphState):
 
+    question = state["rewritten_question"]
+
     answer = state["answer"]
+    
+
     print("\n===== ANSWER VALIDATION =====")
     print(answer)
-    print(state["answer"])
     if "INSUFFICIENT_INFORMATION" in answer:
         print("ROUTE -> FALLBACK")
         print("answer_found = no")
@@ -263,8 +267,47 @@ def answer_validation_node(state: GraphState):
         }
     print("ROUTE -> END")
     print("answer_found = yes")
+    context = "\n\n".join(
+    [doc.page_content for doc in state["documents"][:5]]
+)
+
+    prompt = f"""
+You are an answer grader.
+
+Determine whether the answer sufficiently answers
+the user's question and is supported by the retrieved context.
+
+The answer does NOT need to be perfect.
+
+The answer does NOT need to be extremely detailed.
+
+Return "yes" if the answer is reasonably correct and
+addresses the user's question.
+If the answer is substantially correct,
+even if it is brief or not perfectly worded,
+return yes.
+
+Return "no" only if:
+- the answer is unrelated
+- the answer is incorrect
+- the answer is unsupported by the context
+- the answer fails to answer the question
+
+Question:
+{question}
+
+Retrieved Context:
+{context}
+
+Answer:
+{answer}
+"""
+    result = answer_grader_structured_llm.invoke(
+        prompt
+    )
+    print("Answer Found:", result.answer_found)
     return {
-        "answer_found": "yes"
+        "answer_found": result.answer_found
     }
 
 def fallback_node(state: GraphState):
